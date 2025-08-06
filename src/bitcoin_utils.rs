5ca8985e3af267322c3018c3440b08060741e6d0
aoengin <ahmetoguzengin@hotmail.com>
2025-08-06 19:03:10 +0300

    Update withdrawal signature sequence to enable RBF without locktime

--- src/bitcoin_utils.rs
+++ src/bitcoin_utils.rs
@@ -384,7 +384,7 @@ pub fn sign_withdrawal_signature(
     let txin = TxIn {
         previous_output: *withdrawal_utxo,
         script_sig: ScriptBuf::default(),
-        sequence: Sequence::default(),
+        sequence: Sequence::ENABLE_RBF_NO_LOCKTIME,
         witness: Witness::default(),
     };
 
