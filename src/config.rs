6e79a6231ae1d81bfefe527815ca4f7ff4ba1e4d
atacann <atacaniyidogan@gmail.com>
2025-08-15 16:41:46 +0200

    update test config

--- src/config.rs
+++ src/config.rs
@@ -216,7 +216,7 @@ mod tests {
         assert_eq!(read_config.network, Network::Regtest);
         assert_eq!(
             read_config.bitcoin_config.unwrap().url.as_str(),
-            "http://127.0.0.1/"
+            "http://127.0.0.1:18443/"
         );
 
         fs::remove_file(file_name).unwrap();
