8b6909b1779517970645701f229a30ad4271a687
Ceyhun Şen <ceyhuusen@gmail.com>
2025-08-25 11:00:53 +0300

    chore: Add older deployment values.

--- src/config.rs
+++ src/config.rs
@@ -2,6 +2,7 @@
 //!
 //! Configuration options provided here are used to make a request to Clementine.
 
+use crate::errors::BridgeCliError;
 use bitcoin::{Amount, Network, XOnlyPublicKey};
 use bitcoincore_rpc::{Auth, Client, RpcApi};
 use eyre::Result;
@@ -11,11 +12,13 @@ use serde::Deserialize;
 use std::{fs::File, io::Read, path::PathBuf, str::FromStr, sync::LazyLock};
 use thiserror::Error;
 
-use crate::errors::BridgeCliError;
-
 pub static UNSPENDABLE_XONLY_PUBKEY: LazyLock<XOnlyPublicKey> = LazyLock::new(|| {
     XOnlyPublicKey::from_str("50929b74c1a04954b78b4b6035e97a5e078a5a0f28ec96d547bfee9ace803ac0")
         .unwrap()
+
+    // Old deployments
+    // XOnlyPublicKey::from_str("93c7378d96518a75448821c4f7c8f4bae7ce60f804d03d1f0628dd5dd0f5de51")
+    //     .unwrap()
 });
 
 #[derive(Debug, Error)]
