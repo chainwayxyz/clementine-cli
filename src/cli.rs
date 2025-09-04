296314f6dc3cbc42c647f885656a8db302230099
Ceyhun Şen <ceyhuusen@gmail.com>
2025-09-04 12:18:35 +0300

    Default network as mainnet for subcommands (#98)
    * chore(cli): Make mainnet default for subcommands.
    
    * feat(main): Implement CliNetwork, a wrapper around Bitcoin Network that enables nice help messages.
    
    * chore(cli): Simple log improvement for withdrawal status.
    
    * fix(main): Wrong network type when printing.

--- src/cli.rs
+++ src/cli.rs
@@ -335,7 +335,7 @@ pub async fn withdrawal_status(
     }
 
     println!(
-        "{} Withdrawal status(es) for index {}: \n",
+        "{} Withdrawal status(es) for withdrawal index {}: \n",
         "INFO".bold(),
         withdrawal_index
     );
