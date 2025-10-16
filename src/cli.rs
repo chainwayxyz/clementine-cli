f3170411e444730db21c6124c8d23bc2c0243f5b
ozankaymak <ozan@chainway.xyz>
2025-10-16 15:01:06 +0300

    Refactor wallet creation output to use println! for better readability

--- src/cli.rs
+++ src/cli.rs
@@ -66,20 +66,20 @@ pub fn cli_create_wallet(
     let (address, mnemonic) = create_encrypted_wallet(network, label, purpose, passphrase)?;
 
     let _ = crossterm::terminal::enable_raw_mode();
-    print!("\r\n");
-    print!(
-        "{} Wallet created with address: {}\r\n",
+    println!();
+    println!(
+        "{} Wallet created with address: {}",
         "SUCCESS".bold(),
         address.address_with_prefix()
     );
     if purpose == Purpose::Deposit {
-        print!(
-            "{} Please do not send funds directly to this address!\r\n",
+        println!(
+            "{} Please do not send funds directly to this address!",
             "WARNING".bold(),
         );
     }
 
-    print!("Press any key to continue...\r\n");
+    println!("Press any key to continue...");
     io::stdout().flush().ok();
 
     // Save the cursor *after* the prompt (the natural place you want to end up)
