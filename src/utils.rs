77f13b2cf6d67a78eb9594e001a0fe0d9a0736fb
Ahmet Oguz Engin <85455415+aoengin@users.noreply.github.com>
2025-10-16 15:44:34 +0300

    Add utility function to check if an address belongs to a wallet and update withdrawal logic (#152)

---
+++ src/utils.rs
@@ -0,0 +1,22 @@
+use crate::{
+    BitcoinAddress,
+    config::BridgeCliConfig,
+    errors::BridgeCliError,
+    structs::TaprootAddressWithPrefix,
+    wallet::{Purpose, wallet_utils::address_exists},
+};
+
+pub(crate) fn is_wallet_address(
+    address: &BitcoinAddress,
+    config: &BridgeCliConfig,
+) -> Result<bool, BridgeCliError> {
+    if address.address_type() == Some(bitcoin::AddressType::P2tr) {
+        let address = TaprootAddressWithPrefix::from_string_without_prefix(
+            &address.to_string(),
+            Purpose::Withdrawal,
+            config.network,
+        )?;
+        return address_exists(&address);
+    }
+    Ok(false)
+}
