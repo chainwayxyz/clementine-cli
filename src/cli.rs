7e836531d74d3ce6504f28de05d52f14b4bdb842
Ahmet Oguz Engin <85455415+aoengin@users.noreply.github.com>
2025-10-22 22:55:41 +0300

    Add OutPoint Information to the Incorrect Deposit Message (#170)
    * Enhance deposit error reporting by adding OutPoint information to the incorrect deposit message
    
    * Format OutPoint display in incorrect deposit message for clarity

--- src/cli.rs
+++ src/cli.rs
@@ -451,9 +451,10 @@ pub async fn send_withdrawal_signature(
 }
 
 fn print_incorrect_deposit(utxo: &UtxoInfo, refund_message: &str, block_display: &str) {
+    let outpoint = format!("{}:{}", utxo.txid, utxo.vout);
     println!(
-        "\nIncorrect Deposit\n  TxID:        {}\n  Value:       {}\n  Block:       {}{}",
-        utxo.txid, utxo.value, block_display, refund_message
+        "\nIncorrect Deposit\n  TxID:        {}\n  VOut:        {}\n  OutPoint:    {}\n  Value:       {}\n  Block:       {}{}",
+        utxo.txid, utxo.vout, outpoint, utxo.value, block_display, refund_message
     );
 }
 
