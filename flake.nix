49d3cbfd321d98554356e304e99f1bf862c7fd5e
ozankaymak <ozan@chainway.xyz>
2025-11-26 14:13:17 +0300

    Exclude .DS_Store file from build

--- flake.nix
+++ flake.nix
@@ -75,11 +75,12 @@
               filter = path: type:
                 let base = baseNameOf path;
                 in
-                ! (base == ".git" || base == ".github" || base == "docs" || base == "README.md" 
+                ! (base == ".git" || base == ".github" || base == "docs" || base == "README.md"
                    # Add these lines to exclude local artifacts:
-                   || base == "bridge_cli_config.toml" 
-                   || base == "target" 
-                   || base == "result");
+                   || base == "bridge_cli_config.toml"
+                   || base == "target"
+                   || base == "result"
+                   || base == ".DS_Store");
             };
 
             targetPkgs = if cfg.pkgsCross != null then cfg.pkgsCross else pkgs;
