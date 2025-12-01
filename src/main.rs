18821487a51fd65658fabd2b7f8b21d7c256b559
Ozan Kaymak <92448699+ozankaymak@users.noreply.github.com>
2025-12-01 12:26:19 +0300

    Oguz ozan/improve verify tx recovery (#209)
    Co-authored-by: Ahmet Oguz Engin <85455415+aoengin@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bitcoin::{Network, OutPoint, Transaction, Txid, consensus::deserialize, taproot::Signature};
+use bitcoin::{Network, OutPoint, Txid, taproot::Signature};
 use clap::{Parser, Subcommand};
 use clementine_cli::cli::{
     cli_backup_wallet, cli_create_wallet, cli_generate_withdrawal_signatures,
@@ -10,7 +10,6 @@ use clementine_cli::cli::{
 };
 use clementine_cli::cli_network::{CliNetwork, NETWORK_HELP_MESSAGE, NetworkParser};
 
-use clementine_cli::handle_simple_call;
 use clementine_cli::wallet::should_not_have_purpose;
 use clementine_cli::{
     BitcoinAddress, broadcast_recovery_tx,
@@ -21,6 +20,7 @@ use clementine_cli::{
     wallet::{Purpose, parse_address, parse_taproot_address},
     withdraw,
 };
+use clementine_cli::{handle_simple_call, parse_transaction_hex};
 use colored::Colorize;
 use std::str::FromStr;
 use tracing::level_filters::LevelFilter;
@@ -523,8 +523,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 network,
             } => {
                 let config = handle_simple_call!(BridgeCliConfig::try_parse_config(network.into()));
-                let tx_bytes = handle_simple_call!(hex::decode(&recovery_tx));
-                let recovery_tx: Transaction = handle_simple_call!(deserialize(&tx_bytes));
+                let recovery_tx = handle_simple_call!(parse_transaction_hex(&recovery_tx));
                 let citrea_address = handle_simple_call!(parse_citrea_address(&evm_address));
                 let recovery_taproot_address =
                     handle_simple_call!(TaprootAddressWithPrefix::from_string_with_prefix(
