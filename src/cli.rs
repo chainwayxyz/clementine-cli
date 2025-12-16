5057f18aabf9f5ed6769ce7f4294a9dee5b7d966
aoengin <ahmetoguzengin@hotmail.com>
2025-12-16 20:49:50 +0300

    feat: add get_mempool_txs to api_utils

--- src/cli.rs
+++ src/cli.rs
@@ -17,8 +17,8 @@ use dialoguer::{Input, Select, theme::ColorfulTheme};
 use eyre::Result;
 use url::Url;
 
-use crate::api_utils::get_block_height_for_tx;
-use crate::config::{BitcoinConfig, NetworkConfigs, ToSecretBox};
+use crate::api_utils::{get_block_height_for_tx, get_mempool_txs};
+use crate::config::NetworkConfigs;
 use crate::deposit::{
     get_all_deposit_address_details, get_deposit_address_details_for_deposit_address,
 };
