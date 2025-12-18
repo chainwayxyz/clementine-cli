e8ac7c43b5cd4d1cd474b840a1f2ba96b7974496
aoengin <ahmetoguzengin@hotmail.com>
2025-12-18 15:38:43 +0300

    Improve build script to set BASE_RUSTFLAGS for non-Windows targets and streamline Windows Rust flags

--- flake.nix
+++ flake.nix
@@ -130,9 +130,10 @@
             };
 
             preBuild = ''
-              ${pkgs.lib.optionalString (isWindows) ''
-                export CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUSTFLAGS="$CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUSTFLAGS --remap-path-prefix=$NIX_BUILD_TOP=/build"
-              ''}
+              ${pkgs.lib.optionalString (!isWindows) ''
+              BASE_RUSTFLAGS="-C codegen-units=1 -C debuginfo=0 -C lto=off -C embed-bitcode=no"
+              ${pkgs.lib.optionalString isDarwin ''
+              BASE_RUSTFLAGS="$BASE_RUSTFLAGS -C target-feature=+crt-static"
               BASE_RUSTFLAGS="$BASE_RUSTFLAGS -C link-arg=-Wl,-oso_prefix,$(realpath $NIX_BUILD_TOP)/"
               ''}
               BASE_RUSTFLAGS="$BASE_RUSTFLAGS --remap-path-prefix=$NIX_BUILD_TOP=/build"
@@ -143,9 +144,8 @@
               ''}
 
               ${pkgs.lib.optionalString (isWindows) ''
-                var="CARGO_TARGET_${pkgs.lib.toUpper rustTargetEnv}_RUSTFLAGS"
-                eval "export $var=\"\${$var:-} --remap-path-prefix=\$NIX_BUILD_TOP=/build\""
-              }
+                export CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUSTFLAGS="$CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUSTFLAGS --remap-path-prefix=$NIX_BUILD_TOP=/build"
+              ''}
 
               export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -fdebug-prefix-map=$NIX_BUILD_TOP=/build -fdebug-prefix-map=${src}=/src"
             '';
