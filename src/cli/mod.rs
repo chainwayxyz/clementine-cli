42ccf0b9786516f64e65b085d7cf5c7e49633a28
aoengin <ahmetoguzengin@hotmail.com>
2026-01-20 12:50:35 +0300

    refactor(cli): create new mods for cli related stuff

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,3 +1,6 @@
+pub mod macros;
+pub mod network;
+
 use std::{
     fs, io,
     io::Write,
@@ -36,7 +39,8 @@ use crate::{
     import_wallet_from_private_key,
     secure_display::display_mnemonic_securely,
     secure_types::SecureString,
-    structs::{DepositStatusWithVout, TaprootAddressWithPrefix},
+    deposit::DepositStatusWithVout,
+    wallet::TaprootAddressWithPrefix,
     wallet::{
         Purpose, get_mnemonic_from_wallet, get_private_key_from_wallet,
         mnemonic::prompt_mnemonic,
@@ -50,7 +54,7 @@ use crate::{
 };
 use crate::{
     api_utils::{get_block_height_for_tx, get_mempool_txs},
-    deposit::storage::DepositAddressStorageResult,
+    deposit::DepositAddressStorageResult,
 };
 use crate::{
     config, get_clementine_config_path_with_existence_check, get_clementine_home_dir,
